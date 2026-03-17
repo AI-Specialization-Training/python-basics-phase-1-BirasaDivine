3ac8eaf091032a387db98e5818981a01dcd055a7
Birasa Divine <b.divine@alustudent.com>
2026-03-17 15:33:14 +0200

    Practice 6

---
+++ practice6.py
@@ -0,0 +1,12 @@
+# Write a program that:
+
+# Creates these variables as strings:
+
+pythonnum1 = int("15")
+num2 = int("27") 
+
+# Try printing num1 + num2 without casting — what happens?
+# Now cast both to int and print their sum
+# Cast the sum to a string and print "The answer is: " + the result
+result=str(pythonnum1+num2)
+print(result)

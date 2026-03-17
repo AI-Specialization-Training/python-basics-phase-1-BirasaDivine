8951577919ee29fb70a0280451c6318a16e2b383
Birasa Divine <b.divine@alustudent.com>
2026-03-17 16:21:51 +0200

    Practice 9

---
+++ practice9.py
@@ -0,0 +1,24 @@
+# Write a program that:
+
+# Imports the math module and prints:
+
+# Value of pi
+# Square root of 144
+# Ceiling of 7.3
+# Floor of 7.9
+
+
+# Imports randint and choice from random and:
+
+# Prints a random number between 1 and 100
+# Picks a random name from ["Alice", "Birasa", "Charlie", "Diana"]
+
+import math as m
+print(m.sqrt(m.pi))
+print(m.sqrt(144))
+print(m.ceil(7.3))
+print(m.floor(7.9))
+
+import random as r
+print(r.randint(1,100))
+print(r.choice(["Alice", "Birasa", "Charlie", "Diana"]))
